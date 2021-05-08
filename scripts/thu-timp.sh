be65f8cbcb970171dd12e4463e6456ca3ea47615
igolaizola <11333576+igolaizola@users.noreply.github.com>
2021-05-08 05:12:21 +0200

    Fix week days

--- scripts/thu-timp.sh
+++ scripts/thu-timp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-d=$(date "+%Y-%m-%d" -dnext-tuesday)
+d=$(date "+%Y-%m-%d" -dnext-thursday)
 h=15:15
 curl -X GET "https://maker.ifttt.com/trigger/log/with/key/$IFTTT_KEY?value1=thursday_timp_started%20$d%20$h" > /dev/null 2>&1
 ./bin/timpbot -email "$EMAIL1" -pass "$PASS1" -center "$CENTER1" -activity "$ACTIVITY2" -date "$d" -hour "$h" > /dev/null 2>&1
 curl -X GET "https://maker.ifttt.com/trigger/log/with/key/$IFTTT_KEY?value1=$EMAIL1%20$d%20$h" > /dev/null 2>&1
 h=16:30
-./bin/timpbot -email "$EMAIL6" -pass "$PASS6" -center "$CENTER1" -activity "$ACTIVITY2" -date "$d" -hour "$h" > /dev/null 2>&1
-curl -X GET "https://maker.ifttt.com/trigger/log/with/key/$IFTTT_KEY?value1=$EMAIL6%20$d%20$h" > /dev/null 2>&1
+./bin/timpbot -email "$EMAIL4" -pass "$PASS4" -center "$CENTER1" -activity "$ACTIVITY2" -date "$d" -hour "$h" > /dev/null 2>&1
+curl -X GET "https://maker.ifttt.com/trigger/log/with/key/$IFTTT_KEY?value1=$EMAIL4%20$d%20$h" > /dev/null 2>&1
